e50160283e9b207a57cd3b9b82fedf58dabae308
Arlen <1620339196@qq.com>
2024-03-25 17:44:45 +0800

    Your commit message

---
+++ HansSDK/build/CMakeFiles/HansSDK.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HansSDK.dir/src/Move.cpp.o"
+  "HansSDK"
+  "HansSDK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HansSDK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HansSDK/inc/testSo.h
@@ -0,0 +1,6 @@
+#ifndef _TESTSO_H_
+#define _TESTSO_H_
+
+int add(int a, int b);
+
+#endif // !TESTSO_H
